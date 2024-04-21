c1961f44a7184e9b1b238dcad754d78ba9a1852a
NickKipshidze <kipshidze.nick@gmail.com>
2024-04-21 06:54:15 +0400

    Use settings configuration

--- iptools.py
+++ iptools.py
@@ -1,19 +1,22 @@
-import requests, random
+import requests, random, json
+
+SETTINGS = json.load(open("./settings.json", "r"))
 
 def randip():
     rn = lambda: random.randint(0, 255)
     return f"{rn()}.{rn()}.{rn()}.{rn()}"
 
-def testip(session, IP):
-    target = f"http://{IP}:80/"
-    print(f"[.] Testing {target}")
+def checktarget(session, target):
     try:
         response = session.get(
             target,
             timeout=1
         )
         print("[+] Target is up")
-        open("active.targets", "a").write(f"{response.status_code} {target}\n")
+        open(SETTINGS["output"]["ok-targets"] if response.ok else SETTINGS["output"]["active-targets"], "a").write(
+            f"{response.status_code} {target}\n" if SETTINGS["output"]["append-status-code"] else
+            f"{target}\n"
+        )
         return True
     except requests.ConnectTimeout:
         print("[-] Connection timed out")
@@ -22,3 +25,9 @@ def testip(session, IP):
     except Exception as error:
         print(f"[!] {error}")
     return False
+
+def testip(session, IP):
+    for port in SETTINGS["target"]["open-ports"]:
+        target = f"http://{IP}:{port}/"
+        print(f"[.] Testing {target}")
+        checktarget(session, target)
